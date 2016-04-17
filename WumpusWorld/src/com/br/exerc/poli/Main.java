8abd3acbff4bcac56426c55bd630ebb3447ae259
christoffer <christoffer.luccas@gmail.com>
2016-04-17 17:36:57 -0300

    dfs

--- WumpusWorld/src/com/br/exerc/poli/Main.java
+++ WumpusWorld/src/com/br/exerc/poli/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import com.br.exerc.poli.actions.BFSMatrix;
+import com.br.exerc.poli.actions.DFSMatrix;
 import com.br.exerc.poli.actions.TypeSearches;
 
 /**
@@ -29,12 +30,30 @@ public class Main {
 				System.out.println("2 - Efetuar N testes");
 				System.out.println("0 - SAIR");
 				n = Integer.parseInt(buffer.readLine());
-				TypeSearches exp = new BFSMatrix();
+				if(n == 0) {
+					System.out.println("SAIR");
+					break;
+				}
+				int type = 0;
+				do {
+					System.out.println("1 - BUSCA EM LARGURA");
+					System.out.println("2 - BUSCA EM PROFUNDIDADE");
+					type = Integer.parseInt(buffer.readLine());
+				} while(type < 1 || type > 2);
+				
+				TypeSearches exp;
+				if(type == 1) {
+					exp = new BFSMatrix();
+				}
+				else {
+					exp = new DFSMatrix();
+				}
+					
 				double x;
 				int V = 0, F = 0;
 				switch(n) {
 					case 1:
-						x = 100.0;
+						x = 10.0;
 						for(int i=0; i<(int)x; i++) {
 							System.out.printf("Caso %d\n", (i+1));
 							boolean ans = exp.run();
@@ -43,7 +62,10 @@ public class Main {
 							else
 								F++;
 						}
-						System.out.printf("V(%.2f) F(%.2f) \n", (V/x)*100.0, (F/x)*100.0);
+						System.out.printf("Resultado para %s -> V(%.2f) F(%.2f) \n"
+								,type == 1 ? "Busca em Largura" : "Busca em Profundidade"
+								,(V/x)*100.0
+								,(F/x)*100.0);
 					break;
 					case 2:
 						System.out.println("Informe a quantidade de testes");
@@ -56,7 +78,10 @@ public class Main {
 							else
 								F++;
 						}
-						System.out.printf("V(%.2f) F(%.2f) \n", (V/x)*100.0, (F/x)*100.0);
+						System.out.printf("Resultado para %s -> V(%.2f) F(%.2f) \n"
+								,type == 1 ? "Busca em Largura" : "Busca em Profundidade"
+								,(V/x)*100.0
+								,(F/x)*100.0);
 					break;
 					case 0:
 						System.out.println("SAIR");
