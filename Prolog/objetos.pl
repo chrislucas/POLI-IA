70b8b40384282a5ea10ace0dda6a2a6511c8003b
wiselabs <christoffer.luccas@gmail.com>
2016-05-10 11:48:29 -0300

    estudos sobre objetos em prlog

---
+++ Prolog/objetos.pl
@@ -0,0 +1,14 @@
+/*
+ * Simbolos validos no prolog
+ * Pontuacao .
+ * Conetivos (, virgula - conjuncao), (; ponto e virg - disjuncao) (:-
+ * implicacao)
+ * Letras, digitos e caracteres especiais(+-/<>=:_)
+ *
+*/
+
+posicao(X,Y).
+
+
+% functor
+data(dia, mes, ano).
